1bafbe7e9fc1bb1294e45cb98f84042bd8b2955b
takumi-uezono0 <uezono1998@gmail.com>
2022-12-06 16:55:37 +0900

    create: hedder component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div>
+      <div className="Logo">
+        <p>神兽商店</p>
+        <div id="Tel">
+          お問合わせ
+          <div id="number">0120-xxx-xxxx</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
