3d649ef8c92d0d5bb3567358546fb2c9ef69699e
手島航平 <teshimakouhei@teshimakouheinoMacBook-Air.local>
2022-12-09 15:42:48 +0900

    fix: hedder

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,11 +5,26 @@ const Header = () => {
   return (
     <div>
       <div className="Logo">
-        <p>神兽商店</p>
-        <div id="Tel">
-          お問合わせ
-          <div id="number">0120-xxx-xxxx</div>
+        <div id="h">
+          <p1>PET SHOP</p1>
+          <p></p>
+          <p2>神兽商店</p2>
         </div>
+        <div id="Tel">
+          <p3>お問合わせ:</p3>
+          <p4>0120-XXX-XXXX</p4>
+        </div>
+        <div>
+          <p id="line"></p>
+        </div>
+        <nav>
+          <ul>
+            <li><a href=" ">検索</a></li>
+            <li><a href=" ">お気に入り</a></li>
+            <li><a href=" ">ABOUT</a></li>
+            <li><a href=" ">MENU</a></li>
+          </ul>
+        </nav>
       </div>
     </div>
   );
