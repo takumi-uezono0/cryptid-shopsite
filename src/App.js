8739754f99f20b14d272ac6c58497b9145277c0c
minami nobetsu <nobetsu3@icloud.com>
2022-12-15 00:45:36 +0900

    create:Main2

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header/Header.js";
 import Search from "./components/search/search.js";
 import Footer from "./components/footer/footer";
+import Index from "./components/Main2#3/index";
 
 function App() {
   return (
     <div>
       <Header />
+      <Index />
       <Search />
       <Footer />
     </div>
