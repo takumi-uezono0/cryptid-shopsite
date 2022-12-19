c827101f9daff398585a3dd0893d958b81a2b32e
minami nobetsu <nobetsu3@icloud.com>
2022-12-19 19:50:25 +0900

    fix:main2

--- src/components/Main2#3/index.js
+++ src/components/Main2#3/index.js
@@ -1,39 +1,48 @@
-import { render } from "@testing-library/react";
+{/*import { render } from "@testing-library/react";*/}
 import React from "react";
 import"./subindex.css";
 
 
  
 const Main2 = () => {
+    {/*const img1="../../"*/}
     return(
         <div className="color">
 
 
-            <div id="text1">
+            <div className="text1">
+                
+                    <p id="p1"></p>
+                    <p id="p2"></p>
+                
+                
 
-                {/*<h1><photo1 href="images\font\tou.png"></photo1></h1>*/}
-                {/*<h1><photo2 href="images\font\tou.sub.png"></photo2></h1>*/}
+                {/*<img src="../../../images/tou.png"></img>*/}
+                {/*<h1><img src="../../../images/tou.sub.png"></img></h1>*/}
 
+                {/*<img src={img1}/>*/}
 
-            {/*<img src={"images\photo1.png"}/>*/}
-            <p>弊社は「幻獣と人間の幸せな共生社会の実現」を経営理念に掲げ、</p>
-            <p>お客様とCCA（コンパニオンクリプティッドアニマル）の幸せを願い、 従業員一同日々努力しております。</p>
-            <p>私たちは、CCAを取り巻く課題や問題に対しても一つ一つ向き合いながら、</p>
-            <p>幻獣と人間の幸せな共生の為に、全員一丸となって取り組んでまいります。</p>
-        </div>
+                {/*<h1><img src="../../../images/photo1.png"></img></h1>*/}
+             <p>弊社は「幻獣と人間の幸せな共生社会の実現」を経営理念に掲げ、</p>
+             <p>お客様とCCA（コンパニオンクリプティッドアニマル）の幸せを願い、 従業員一同日々努力しております。</p>
+             <p>私たちは、CCAを取り巻く課題や問題に対しても一つ一つ向き合いながら、</p>
+             <p>幻獣と人間の幸せな共生の為に、全員一丸となって取り組んでまいります。</p>
+           </div>
 
     
 
-        <div id="text2">
+            <div className="text2">
+                    <p id="p3"></p>
+                    <p id="p4"></p>
 
-           {/* <h1><photo3 href="images\font\o.png"></photo3></h1>*/}
-            {/*<h1><photo4 href="images\font\o.sub.png"></photo4></h1>*/}
+               {/* <h1><img src="images\font\o.png"></img></h1>*/}
+               {/*<h1><img src="images\font\o.sub.png"></img></h1>*/}
 
-        {/*<img src={"images\photo2.png"}/>*/}
+               {/*<img src={"images\photo2.png"}/>*/}
                 <p>幻獣たちとのペットライフはお迎えいただいてからが本番です。</p>
                 <p>初めての不安を幸せに変えるため、弊社が全力でサポートいたします。</p>
 
-                <ul>
+                <ul id="text-left">
                         <li>幻獣たちそれぞれの生態や捕獲に至った経緯を記した「幻獣攻略マニュアル本」をプレゼント</li>
                         <li>24時間365日電話での健康相談・緊急相談サービス</li>
 
@@ -41,19 +50,21 @@ const Main2 = () => {
             </div>
             
             
-            <div id="text3">
+            <div className="text3">
+                 <p id="p5"></p>
+                 <p id="p6"></p>
 
-            {/*<h1><photo5 href="images\font\ken.png"></photo5></h1>*/}
-            {/*<h1><photo6 href="images\font\ken.sub.png"></photo6></h1>*/}
+               {/*<h1><photo5 href="images\font\ken.png"></photo5></h1>*/}
+               {/*<h1><photo6 href="images\font\ken.sub.png"></photo6></h1>*/}
 
-            {/*<img src={"images\photo3.png"}/>*/}
+               {/*<img src={"images\photo3.png"}/>*/}
                 <p>幻獣たちはとても強い生き物ですが、環境の変化に敏感な個体も多く、本来飼育には適しておりません。</p>
                 <p>ですが、専門知識がある私たちが定期的に健康診断を行わせていただくことで、</p>
                 <p>元気に暮らしていけるようにサポートをさせていただきます。</p>
             </div>
             
     
-            </div>
+</div>
     );
 }
 export default Main2
